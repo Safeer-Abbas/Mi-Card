f264d22d9313fe1604ca53f6e88334bddaf9ee25
Safeer Abbas <safeerabbas3@gmail.com>
2020-11-18 09:29:41 -0500

    Mi-Card initial files

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    miCard(),
+  );
+}
+
+class miCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Container(),
+      ),
+    );
+  }
+}
