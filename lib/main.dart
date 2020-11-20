fe879f1125c5426b231ae2e4943e9c0bf1f6aa2c
Safeer Abbas <safeerabbas3@gmail.com>
2020-11-20 12:18:56 -0500

    Added Padding, Margins, and Containers

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-import 'dart:html';
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,21 +13,72 @@ class miCard extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
-            children: [
-              Center(
-                child: CircleAvatar(
-                  backgroundImage: ExactAssetImage('assets/Safeer_Abbas.png'),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  backgroundImage:
+                      ExactAssetImage('assets/Images/Safeer_Abbas.png'),
                   radius: 100,
                 ),
-              ),
-              Text("Hello There Safee")
-
-
-            ],)
-        ),
+                Text("Safeer Abbas",
+                    style: TextStyle(
+                        fontFamily: 'Pacifico',
+                        fontSize: 40.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold)),
+                Text(
+                  "FLUTTER DEVELOPER",
+                  style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 30.0,
+                      color: Colors.teal.shade100,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 2.5),
+                ),
+                Container(
+                  //Container 1
+                  margin: EdgeInsets.fromLTRB(25.0, 10.0, 25.0, 10.0),
+                  padding: EdgeInsets.all(10.0),
+                  color: Colors.white,
+                  child: Row(
+                    children: [
+                      Icon(Icons.phone, color: Colors.teal),
+                      SizedBox(width: 10.0),
+                      Text(
+                        "123 456 7890",
+                        style: TextStyle(
+                          color: Colors.teal[900],
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  //Container 1
+                  margin: EdgeInsets.fromLTRB(25.0, 10.0, 25.0, 10.0),
+                  padding: EdgeInsets.all(10.0),
+                  color: Colors.white,
+                  child: Row(
+                    children: [
+                      Icon(Icons.email, color: Colors.teal),
+                      SizedBox(width: 10.0),
+                      Text(
+                        "test@gmail.com",
+                        style: TextStyle(
+                          color: Colors.teal[900],
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            )),
       ),
-    ); 
+    );
   }
 }
-  
